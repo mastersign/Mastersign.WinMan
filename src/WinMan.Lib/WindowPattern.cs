eb1fe0dac5c64dbea09503ad247bbccc0c7514ee
Tobias Kiertscher <kiertscher@th-brandenburg.de>
2017-06-06 09:23:30 +0200

    fixed retrieving restore command for new window pattern

--- src/WinMan.Lib/WindowPattern.cs
+++ src/WinMan.Lib/WindowPattern.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Management;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -37,16 +38,65 @@ namespace Mastersign.WinMan
             return true;
         }
 
-        public static WindowPattern FromWindow(WindowWrapper w)
-            => new WindowPattern()
+        private static void GetCommandArgs(string commandLine, out string command, out string commandArgs)
+        {
+            command = null;
+            commandArgs = null;
+            if (commandLine == null) return;
+            var respectSpace = true;
+            for (int i = 0; i < commandLine.Length; i++)
+            {
+                if (commandLine[i] == '"') respectSpace = !respectSpace;
+                if (commandLine[i] == ' ' && respectSpace)
                 {
-                    TitlePattern = w.Title,
-                    WindowClassPattern = w.WindowClass,
-                    ProcessFileName = w.Process?.MainModule?.FileName,
-                    Command = w.Process?.StartInfo?.FileName,
-                    CommandArgs = w.Process?.StartInfo?.Arguments,
-                    WorkingDir = w.Process?.StartInfo?.WorkingDirectory
-                };
+                    command = commandLine.Substring(0, i - 1).Replace("\"", "");
+                    commandArgs = commandLine.Substring(i + 1);
+                    break;
+                }
+            }
+        }
+
+        public static WindowPattern FromWindow(WindowWrapper w)
+        {
+            var process = w.Process;
+            string command = null;
+            string commandArgs = null;
+            if (process != null)
+            {
+                var wmiQuery = $"SELECT CommandLine FROM Win32_Process WHERE ProcessId={process.Id}";
+                var objectSearcher = new ManagementObjectSearcher(wmiQuery);
+                var results = objectSearcher.Get();
+                try
+                {
+                    foreach (var o in results)
+                    {
+                        foreach (var p in o.Properties)
+                        {
+                            if (p.Name == "CommandLine")
+                            {
+                                GetCommandArgs(p.Value as string, out command, out commandArgs);
+                            }
+                        }
+                        break;
+                    }
+                }
+                finally
+                {
+                    results.Dispose();
+                    objectSearcher.Dispose();
+                }
+            }
+
+            return new WindowPattern()
+            {
+                TitlePattern = w.Title,
+                WindowClassPattern = w.WindowClass,
+                ProcessFileName = w.Process?.MainModule?.FileName,
+                Command = command,
+                CommandArgs = commandArgs,
+                WorkingDir = w.Process?.StartInfo?.WorkingDirectory
+            };
+        }
 
         public WindowWrapper[] Discover() => WindowWrapper.AllWindows().Where(w => IsMatch(w)).ToArray();
 
