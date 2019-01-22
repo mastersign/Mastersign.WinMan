262d6385369cab2333576170fb37d4d8b1c85d6b
Tobias Kiertscher <kiertscher@th-brandenburg.de>
2019-01-22 09:48:47 +0000

    update interface GUIDs for Windows 1809

--- src/WinMan.Lib/vendor/VirtualDesktop/Interop/IApplicationViewCollection.cs
+++ src/WinMan.Lib/vendor/VirtualDesktop/Interop/IApplicationViewCollection.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 namespace WindowsDesktop.Interop
 {
 	[ComImport]
-	[Guid("2c08adf0-a386-4b35-9250-0fe183476fcc")]
+	[Guid("1841c6d7-4f9d-42c0-af41-8747538f10e5")]
 	[InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
 	public interface IApplicationViewCollection
 	{
