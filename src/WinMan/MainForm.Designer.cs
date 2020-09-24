8c6dd21b0301312a93d94d4ee57b33bd754aed01
Tobias Kiertscher <dev@mastersign.de>
2020-09-24 08:15:37 +0000

    always start WinMan UI in the center of the screen

--- src/WinMan/MainForm.Designer.cs
+++ src/WinMan/MainForm.Designer.cs
@@ -2817,6 +2817,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "WinMan";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ClosingHandler);
             this.Load += new System.EventHandler(this.FormLoadHandler);
