2f32307bbcecdd466e83d4befa151cf024c1974d
Tobias Kiertscher <kiertscher@th-brandenburg.de>
2023-04-04 08:26:55 +0000

    improved automation scripts

--- auto/Add-IconsToRelease.ps1
+++ auto/Add-IconsToRelease.ps1
@@ -7,4 +7,8 @@ if (!(Test-Path $target)) {
     exit 1
 }
 
+Read-Host "Deactivate Realtime Antivirus to allow modifying the EXE without interruptions and press ENTER"
+
 & "$thisDir\InsertIcons.exe" $target "$thisDir\icons.txt"
+
+Read-Host "Reactivate Realtime Antivirus again! And press ENTER"
